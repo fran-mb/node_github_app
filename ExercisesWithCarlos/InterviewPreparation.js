b40ca6d1cea8d422073d0d20585317069ba14d74
Francieli Machado Brighenti <96430830+fran-mb@users.noreply.github.com>
2022-01-23 18:27:32 +0000

    day5 and interview preparation

---
+++ ExercisesWithCarlos/InterviewPreparation.js
@@ -0,0 +1,29 @@
+/* 
+To be ready for an interview I need:
+
+TOOLS
+
+1. Commnand line
+2. Github and git
+3. npm
+4. Travis CI (Continuou Integration)
+
+PROGRAMMING LANGUAGES
+
+CORE:
+1.HTML
+2.CSS
+3.JS
+
+4. Basics on React
+5. JSON
+6. CSS Pre-processors as SASS
+
+OTHERS (good to have)
+
+1. Agile Basics and scrum
+2. Testing (jest and others...)
+3. To know what a11y (accessibility), i18n (internationalisation) and CI/CD is
+4. Ajax (Asynchrounous JS)
+5. Client/Server communication libraries
+*/
