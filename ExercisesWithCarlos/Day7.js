8c0aca0e39c18bf6d6c7d27e1a9ff5684e33102c
Francieli Machado Brighenti <96430830+fran-mb@users.noreply.github.com>
2022-02-06 13:53:32 +0000

    update exercise description

--- ExercisesWithCarlos/Day7.js
+++ ExercisesWithCarlos/Day7.js
@@ -1,46 +1,66 @@
 // RECIPES APP
 
 /* 
-    - The goal of this exercise is to implement a library called recipes_utils that will provide a set of functions
-      to support the following application. 
+    - The goal of this exercise is to implement a set of functions to support the following application. 
     - Implement the functions used in the application so that they meet the expected result.
     - Create a feature branch a PR with your changes.
-    - Use JS ES6 features as (import, export, arrow functions, spread operator, map, forEach and find)
+    - Use JS ES6 features as (arrow functions, spread operator, map, forEach and find)
     - See here the 10 most useful ES6 features: https://blog.pragmatists.com/top-10-es6-features-by-example-80ac878794bb
 */
 
-// Imports
+// Functions - what needs to be implemented
 
-import { sayWelcome, showRecipesNumber } from './lib/recipes_utils';
+const sayWelcome = () => {
+  console.log("Welcome to my restaurant");
+};
+
+const showRecipesNumber = (recipes) => recipes.length;
 
 // Data
 const gazpacho = {
-  name: 'Gazpacho',
-  recipe: ['tomato', 'cucumber', 'pepper', 'water', 'garlic', 'olive oil', 'vinegar', 'bread'],
+  name: "Gazpacho",
+  recipe: [
+    "tomato",
+    "cucumber",
+    "pepper",
+    "water",
+    "garlic",
+    "olive oil",
+    "vinegar",
+    "bread",
+  ],
   rating: 10,
 };
 
 const brigadeiro = {
-  name: 'Brigadeiro',
-  recipe: ['tomato', 'cucumber', 'pepper', 'water', 'garlic', 'olive oil', 'vinegar'],
+  name: "Brigadeiro",
+  recipe: [
+    "tomato",
+    "cucumber",
+    "pepper",
+    "water",
+    "garlic",
+    "olive oil",
+    "vinegar",
+  ],
   rating: 9,
 };
 
 const paella = {
-  name: 'Paella',
-  recipe: ['rice', 'seafood', 'tomato', 'peeper', 'garlic', 'water', 'potato'],
+  name: "Paella",
+  recipe: ["rice", "seafood", "tomato", "peeper", "garlic", "water", "potato"],
   rating: 8,
 };
 
 const tortilla = {
-  name: 'Tortilla',
-  recipe: ['potatoes', 'eggs', 'olive oil', 'onion'],
+  name: "Tortilla",
+  recipe: ["potatoes", "eggs", "olive oil", "onion"],
   rating: 7,
 };
 
 const torrijas = {
-  name: 'Torrijas',
-  recipe: ['bread', 'eggs', 'milk', 'honey'],
+  name: "Torrijas",
+  recipe: ["bread", "eggs", "milk", "honey"],
   rating: 6,
 };
 
@@ -55,4 +75,4 @@ showLowestRatingRecipe(recipes); // Expected output: 'Tortilla. Rating: 7'
 addRecipe(recipes, torrijas); // Expected output: 'Recipe for Torrias has been added'
 showRecipesNumber(recipes); // Expected output: 'There are 5 recipes'
 showAllRecipesNames(recipes); // Expected output: 'Gazpacho, Brigadeiro, Paella, Tortilla, Torrijas'
-findRecipe(recipes, 'Tortilla'); // Expected output: 'Tortilla recipe: 'potatoes', 'eggs', 'olive oil', 'onion''
+findRecipe(recipes, "Tortilla"); // Expected output: 'Tortilla recipe: 'potatoes', 'eggs', 'olive oil', 'onion''
